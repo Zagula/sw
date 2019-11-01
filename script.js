860a604aed1c9d91dd7c96ac2a86b3a2fcd8b074
Andrei_Tsiarnouski <Andrei_Tsiarnouski@epam.com>
2019-11-01 17:47:57 +0300

    1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker.register("sw.js").then(function(registration) {
-            console.log("Successful", registration.scope);
+            console.log("Successful", registration);
         }).catch(function(err) {
             console.log("Failed: ", err);
         });
