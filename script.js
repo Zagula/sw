9c6bb8e42fa1b7e2f6c2894b218c9e85b3f69af4
Andrei_Tsiarnouski <Andrei_Tsiarnouski@epam.com>
2019-11-01 17:46:36 +0300

    1

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-console.log('script загружен');
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker.register("sw.js").then(function(registration) {
-            console.log("ServiceWorker registration successful with scope: ", registration.scope);
+            console.log("Successful", registration.scope);
         }).catch(function(err) {
-            console.log("ServiceWorker registration failed: ", err);
+            console.log("Failed: ", err);
         });
     });
 };
--- sw.js
+++ sw.js
@@ -2,12 +2,15 @@ console.log('sw.js загружен')
 
 self.addEventListener('install', (event) => {
     console.log('Установлен');
+    console.dir(event);
 });
 
 self.addEventListener('activate', (event) => {
     console.log('Активирован');
+    console.dir(event);
 });
 
 self.addEventListener('fetch', (event) => {
     console.log('Происходит запрос на сервер');
+    console.dir(event);
 });
